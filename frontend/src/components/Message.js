d880004616b9ab45f284c92ba70b4280408a3664
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-04-10 01:45:09 -0400

    Message component ok

---
+++ frontend/src/components/Message.js
@@ -0,0 +1,24 @@
+import Swal from 'sweetalert2'
+
+const Message = ({ icon, title, position = 'top-end' }) => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: position,
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
+    return (
+        Toast.fire({
+            icon: icon,
+            title: title
+        })
+    )
+}
+
+export default Message
