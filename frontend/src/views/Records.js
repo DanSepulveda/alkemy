05eef5178e79c0f884e215948fcc34326a17c9f8
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-04-12 09:39:35 -0400

    Record component completed

--- frontend/src/views/Records.js
+++ frontend/src/views/Records.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import Navbar from '../components/Navbar'
 import Resume from '../components/Resume'
 import MainSection from '../components/MainSection'
@@ -7,13 +8,17 @@ import { useLocation } from 'react-router-dom'
 const Records = () => {
     const path = useLocation().pathname
 
+    const component = path === '/'
+        ? <Resume />
+        : <Transactions />
+
     return (
-        <>
+        <Fragment>
             <Navbar />
             <MainSection>
-                {path === '/' ? <Resume /> : <Transactions />}
+                {component}
             </MainSection>
-        </>
+        </Fragment>
     )
 }
 
